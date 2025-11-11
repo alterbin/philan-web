f0aad30ed1ff3959045ef991a1ac1e1857fea938
kunle-didunyemi <senatorkay2@gmail.com>
2025-11-11 10:31:19 +0100

    remove webpack

--- next.config.ts
+++ next.config.ts
@@ -45,13 +45,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 export default pwa(nextConfig);
